594f5005972e788a8641b9fe45bc8702bb26a8fa
Tomas Mūžas <m_tomas@inbox.lt>
2019-03-07 23:04:53 +0200

    Add separate independent .NET project with default controller

---
+++ BackendApartmentReservation/BackendApartmentReservation/Controllers/FlatsController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApartmentReservation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class FlatsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/flats")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] { "flat1", "flat2" };
+        }
+    }
+}
