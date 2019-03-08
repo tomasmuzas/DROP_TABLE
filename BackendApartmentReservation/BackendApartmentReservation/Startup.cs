206adfcd31b02c07c249ace1b42c1fcd00607733
Tomas Mūžas <m_tomas@inbox.lt>
2019-03-08 18:24:46 +0200

    Allow CORS from any origin

--- BackendApartmentReservation/BackendApartmentReservation/Startup.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Startup.cs
@@ -18,6 +18,10 @@ namespace BackendApartmentReservation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder => builder.AllowAnyOrigin());
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
