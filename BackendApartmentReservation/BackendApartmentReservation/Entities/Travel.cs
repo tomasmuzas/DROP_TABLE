113659bf42d142073089682b7d00faa949f1bab9
Martynas Struckus <Martynas.Struckus@bentley.com>
2019-03-12 12:06:51 +0200

    Add travel entity and travel option status enum

---
+++ BackendApartmentReservation/BackendApartmentReservation/Entities/Travel.cs
@@ -0,0 +1,18 @@
+﻿using BackendApartmentReservation.Utilities;
+using System;
+
+namespace BackendApartmentReservation.Entities
+{
+    public class Travel
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+        // public Office DestinationOffice { get; set; }
+        public string DestinationOffice { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public TravelOptionStatus TripTickets { get; set; }
+        public TravelOptionStatus CarRental { get; set; }
+        public TravelOptionStatus Apartments { get; set; }
+    }
+}
