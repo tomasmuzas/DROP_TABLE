620dc831ab414dff00309749aae3863a839825bb
Ieva <racylaite.ieva@gmail.com>
2019-03-13 19:38:39 +0200

    Add Id

--- BackendApartmentReservation/BackendApartmentReservation/Entities/Authentication.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Entities/Authentication.cs
@@ -7,6 +7,7 @@ namespace BackendApartmentReservation.Entities
 {
     public class Authentication
     {
+        public int Id { get; set; }
         public int ProfileId { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
