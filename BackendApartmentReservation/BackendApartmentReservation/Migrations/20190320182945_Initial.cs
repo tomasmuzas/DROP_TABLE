63bc74812d7769e5eea80e93e7ee500c516795f9
Tomas Mūžas <m_tomas@inbox.lt>
2019-03-20 20:31:11 +0200

    Add Initial Migration and connection strings

---
+++ BackendApartmentReservation/BackendApartmentReservation/Migrations/20190320182945_Initial.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendApartmentReservation.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Apartments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Apartments", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trips",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Cost = table.Column<double>(nullable: false),
+                    DestinationOffice = table.Column<string>(nullable: true),
+                    DepartureDate = table.Column<DateTime>(nullable: false),
+                    ReturnDate = table.Column<DateTime>(nullable: false),
+                    TripTickets = table.Column<int>(nullable: false),
+                    CarRental = table.Column<int>(nullable: false),
+                    Apartments = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trips", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ManagerId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Office = table.Column<string>(nullable: true),
+                    DbGroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employees_Groups_DbGroupId",
+                        column: x => x.DbGroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_DbGroupId",
+                table: "Employees",
+                column: "DbGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_ManagerId",
+                table: "Groups",
+                column: "ManagerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Employees_ManagerId",
+                table: "Groups",
+                column: "ManagerId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Groups_DbGroupId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Apartments");
+
+            migrationBuilder.DropTable(
+                name: "Trips");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
--- BackendApartmentReservation/BackendApartmentReservation/Startup.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Autofac;
 
+using BackendApartmentReservation.Database;
 using BackendApartmentReservation.Infrastructure.Containers;
 using BackendApartmentReservation.Infrastructure.Exceptions;
 using BackendApartmentReservation.Infrastructure.Logging;
@@ -7,6 +8,7 @@ using BackendApartmentReservation.Infrastructure.Logging;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -27,6 +29,10 @@ namespace BackendApartmentReservation
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddMvcOptions(options => options.Filters.Add(new MethodCallLoggingFilter()))
                 .AddMvcOptions(options => options.Filters.Add(new GlobalExceptionFilter()));
+
+            var connectionString = Configuration.GetConnectionString("DatabaseContext");
+            services.AddDbContext<DatabaseContext>(options => 
+                options.UseSqlServer(connectionString));
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
