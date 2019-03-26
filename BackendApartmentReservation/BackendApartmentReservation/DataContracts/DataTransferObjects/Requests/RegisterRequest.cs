56642821b7fbab0afecfd3d227a0d123bfc45f56
Andrej Kuznecov <an.kuznecovas@gmail.com>
2019-03-26 12:00:08 +0200

    add DataContracts folder and create a RegisterRequest + UserRole for future

---
+++ BackendApartmentReservation/BackendApartmentReservation/DataContracts/DataTransferObjects/Requests/RegisterRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendApartmentReservation.DataContracts.DataTransferObjects.Requests
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Office { get; set; }
+    }
+}
---
+++ BackendApartmentReservation/BackendApartmentReservation/DataContracts/UserRoles.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendApartmentReservation.DataContracts
+{
+    public enum UserRoles
+    {
+        Admin,
+        User
+    }
+
+    public static class UserRoleString
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
