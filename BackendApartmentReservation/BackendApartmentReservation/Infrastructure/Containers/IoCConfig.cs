eb3b3eaaa8ccb1c5228ed9eea44c5c8a4251627c
Ieva <racylaite.ieva@gmail.com>
2019-04-03 23:45:17 +0300

    Remove not needed files

--- BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
@@ -1,10 +1,7 @@
 ﻿using Autofac;
-<<<<<<< HEAD
-using BackendApartmentReservation.Managers;
-=======
 
+using BackendApartmentReservation.Managers;
 using BackendApartmentReservation.Infrastructure.Logging;
->>>>>>> 84ed0fd37a8ec8214c0155573e5c9ac7d3e6421a
 using BackendApartmentReservation.Repositories;
 
 namespace BackendApartmentReservation.Infrastructure.Containers
@@ -13,14 +10,10 @@ namespace BackendApartmentReservation.Infrastructure.Containers
     {
         protected override void Load(ContainerBuilder builder)
         {
-<<<<<<< HEAD
             builder.RegisterType<EmployeeManager>()
                 .As<IEmployeeManager>();
             builder.RegisterType<EmployeeRepository>()
                 .As<IEmployeeRepository>();
-=======
-            builder.RegisterType<ProfileRepository>().As<IProfileRepository>();
->>>>>>> 84ed0fd37a8ec8214c0155573e5c9ac7d3e6421a
         }
     }
 }
