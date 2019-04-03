f12528d20e7d124c313fae98d8bfed47b13a57ad
Ieva <racylaite.ieva@gmail.com>
2019-04-03 20:49:18 +0300

    Change repository

--- BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
@@ -1,4 +1,7 @@
 ﻿using Autofac;
+
+using BackendApartmentReservation.Infrastructure.Logging;
+using BackendApartmentReservation.Repositories;
 using BackendApartmentReservation.Managers;
 
 namespace BackendApartmentReservation.Infrastructure.Containers
@@ -7,6 +10,7 @@ namespace BackendApartmentReservation.Infrastructure.Containers
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<ProfileRepository>().As<IProfileRepository>();
             builder.RegisterType<EmployeeManager>()
                 .As<IEmployeeManager>();
         }
--- BackendApartmentReservation/BackendApartmentReservation/Repositories/IProfileRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Repositories/IProfileRepository.cs
@@ -1,15 +1,16 @@
 ﻿using BackendApartmentReservation.Database.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace BackendApartmentReservation.Repositories
 {
     public interface IProfileRepository
     {
-        IEnumerable<DbEmployee> GetAll();
-        DbEmployee GetById(int EmployeeID);
-        IEnumerable<DbEmployee> GetByFirstNameLastName(string FirstName, string LastName);
-        DbEmployee Create(DbEmployee DbEmployee);
-        bool Update(int id, DbEmployee DbEmployee);
-        bool Delete(int EmployeeID);
+        Task<IEnumerable<DbEmployee>> GetAll();
+        Task<DbEmployee> GetById(int employeeID);
+        Task Create(DbEmployee dbEmployee);
+        Task Update(DbEmployee dbEmployee);
+        Task Delete(DbEmployee dbEmployee);
+        Task SaveChanges();
     }
 }
