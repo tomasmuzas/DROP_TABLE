8e93a38d5a239374f8f39ccb9489639a609ddab5
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-04 21:25:51 +0300

    Add managers for checklist

---
+++ BackendApartmentReservation/BackendApartmentReservation/Managers/FlightReservationInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendApartmentReservation.Managers
+{
+    public class FlightReservationInfo
+    {
+        public bool Required { get; set; }
+
+        public string FlightNumber { get; set; }
+    }
+}
