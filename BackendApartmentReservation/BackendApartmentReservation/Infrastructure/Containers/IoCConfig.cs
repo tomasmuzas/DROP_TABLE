40efe6a39d487d72eb4773e8d10944cc9ee3c2ff
Tomas Mūžas <muzas.tomas@gmail.com>
2019-04-05 21:17:49 +0300

    Update IoC config after all merges

--- BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Infrastructure/Containers/IoCConfig.cs
@@ -1,19 +1,17 @@
 ﻿using Autofac;
-
-using BackendApartmentReservation.Infrastructure.Logging;
-using BackendApartmentReservation.Repositories;
 using BackendApartmentReservation.Managers;
-using BackendApartmentReservation.Repositories;
 
 namespace BackendApartmentReservation.Infrastructure.Containers
 {
+    using Repositories;
+
     public class IoCConfig : Module
     {
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<EmployeeManager>()
                 .As<IEmployeeManager>();
-            builder.RegisterType<ProfileRepository>().As<IProfileRepository>();
+            builder.RegisterType<EmployeeRepository>().As<IEmployeeRepository>();
         }
     }
 }
