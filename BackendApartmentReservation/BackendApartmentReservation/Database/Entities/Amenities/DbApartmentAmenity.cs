1c9c71b023749be66759c4dbb4d677d956abbb5d
Andrej Kuznecov <an.kuznecovas@gmail.com>
2019-04-15 13:24:17 +0300

    Add amenities

---
+++ BackendApartmentReservation/BackendApartmentReservation/Database/Entities/Amenities/DbApartmentAmenity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendApartmentReservation.Database.Entities.Reservations;
+
+namespace BackendApartmentReservation.Database.Entities.Amenities
+{
+    public class DbApartmentAmenity : AmenityBase
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DbApartmentReservation ApartmentReservation { get; set; }
+    }
+}
