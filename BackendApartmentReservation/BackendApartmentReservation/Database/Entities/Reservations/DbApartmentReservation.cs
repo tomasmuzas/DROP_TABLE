0596d3cd3779f92e35cd132456d285a896a3dfdb
Andrej Kuznecov <an.kuznecovas@gmail.com>
2019-04-15 13:25:21 +0300

    Add database living spaces reservations

---
+++ BackendApartmentReservation/BackendApartmentReservation/Database/Entities/Reservations/DbApartmentReservation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendApartmentReservation.Database.Entities.Amenities;
+
+namespace BackendApartmentReservation.Database.Entities.Reservations
+{
+    public class DbApartmentReservation
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+    }
+}
