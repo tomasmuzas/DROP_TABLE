a14d1dc8effc5ec05d71521b615af7fcd68dcfb1
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-18 20:46:10 +0300

    Add office repository

---
+++ BackendApartmentReservation/BackendApartmentReservation/Repositories/OfficeRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace BackendApartmentReservation.Repositories
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Database;
+    using Database.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class OfficeRepository : IOfficeRepository
+    {
+        private readonly DatabaseContext _db;
+
+        public OfficeRepository(DatabaseContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<DbOffice>> GetAllOffices()
+        {
+            return await _db.Offices.ToListAsync();
+        }
+    }
+}
