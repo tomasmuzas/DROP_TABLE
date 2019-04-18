6653c940543a8da79a5ab4a2db9e538a7961a349
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-18 20:47:16 +0300

    Add office controller

---
+++ BackendApartmentReservation/BackendApartmentReservation/Controllers/OfficeController.cs
@@ -0,0 +1,27 @@
+﻿namespace BackendApartmentReservation.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using DataContracts.DataTransferObjects.Responses;
+    using Managers;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api")]
+    [ApiController]
+    public class OfficeController: ControllerBase
+    {
+        private readonly IOfficeManager _officeManager;
+
+        public OfficeController(IOfficeManager officeManager)
+        {
+            _officeManager = officeManager;
+        }
+
+        [HttpGet]
+        [Route("offices")]
+        public async Task<IEnumerable<OfficeInfoResponse>> GetAllOffices()
+        {
+            return await _officeManager.GetAllOffices();
+        }
+    }
+}
