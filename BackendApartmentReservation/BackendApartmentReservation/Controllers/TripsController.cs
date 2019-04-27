280b5946d875656f0df5325690d4feab84ac3ca8
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-27 22:41:18 +0300

    Add trip information controller

---
+++ BackendApartmentReservation/BackendApartmentReservation/Controllers/TripsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using BackendApartmentReservation.Database.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApartmentReservation.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("trips")]
+        public async Task<IEnumerable<DbTrip>> Get()
+        {
+            var travelMock = new DbTrip()
+            {
+                Id = 1,
+                DepartureDate = DateTime.UtcNow.AddDays(2),
+                ReturnDate = DateTime.UtcNow.AddDays(5),
+                DestinationOffice = new DbOffice()
+            };
+
+            return await Task.FromResult(new List<DbTrip> { travelMock });
+        }
+    }
+}
---
+++ BackendApartmentReservation/BackendApartmentReservation/DataContracts/DataTransferObjects/IntermediaryDTOs/BasicPersonalChecklistInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace BackendApartmentReservation.DataContracts.DataTransferObjects.IntermediaryDTOs
+{
+    using Responses;
+
+    public class BasicPersonalChecklistInfo
+    {
+        public EmployeeInfo Employee{ get; set; }
+
+        public bool IsFlightRequired { get; set; }
+
+        public bool IsApartmentRequired { get; set; }
+
+        public bool IsCarRentRequired { get; set; }
+    }
+}
