bf1e4ba1241dfb11364f9709b840752853b185b6
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-27 22:40:41 +0300

    Add trip information manager

---
+++ BackendApartmentReservation/BackendApartmentReservation/Managers/ITripInformationManager.cs
@@ -0,0 +1,10 @@
+﻿namespace BackendApartmentReservation.Managers
+{
+    using System.Threading.Tasks;
+    using DataContracts.DataTransferObjects.Responses;
+
+    public interface ITripInformationManager
+    {
+        Task<BasicTripInformationResponse> GetBasicTripInformation(string tripId);
+    }
+}
---
+++ BackendApartmentReservation/BackendApartmentReservation/Managers/TripInformationManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading.Tasks;
+
+namespace BackendApartmentReservation.Managers
+{
+    using System.Linq;
+    using DataContracts.DataTransferObjects.IntermediaryDTOs;
+    using DataContracts.DataTransferObjects.Responses;
+    using Repositories;
+
+    public class TripInformationManager : ITripInformationManager
+    {
+        private readonly ITripRepository _tripRepository;
+
+        public TripInformationManager(
+            ITripRepository tripRepository,
+            IEmployeeManager employeeManager)
+        {
+            _tripRepository = tripRepository;
+        }
+
+        public async Task<BasicTripInformationResponse> GetBasicTripInformation(string tripId)
+        {
+            var trip = await _tripRepository.GetTrip(tripId);
+
+            var checklists = await _tripRepository.GetTripChecklistsWithEmployees(tripId);
+
+            var checklistInformations = checklists
+                .Select(c => new BasicPersonalChecklistInfo
+                {
+                    Employee = new EmployeeInfo
+                    {
+                        Id = c.Employee.ExternalEmployeeId,
+                        Email = c.Employee.Email,
+                        FirstName = c.Employee.FirstName,
+                        LastName = c.Employee.LastName
+                    },
+                    IsApartmentRequired = c.LivingPlace != null,
+                    IsFlightRequired = c.Flight != null,
+                    IsCarRentRequired = c.Car != null
+                })
+                .ToList();
+
+            return new BasicTripInformationResponse
+            {
+                TripId = tripId,
+                StartTime = trip.DepartureDate,
+                EndTime = trip.ReturnDate,
+
+                ChecklistInfos = checklistInformations
+            };
+        }
+    }
+}
