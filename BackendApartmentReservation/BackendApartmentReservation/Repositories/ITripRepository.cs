651b8e4acb7aa6a32733ef965798b9c4b249fdc3
Tomas Mūžas <m_tomas@inbox.lt>
2019-04-27 22:40:11 +0300

    Add trip repository

---
+++ BackendApartmentReservation/BackendApartmentReservation/Repositories/ITripRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendApartmentReservation.Repositories
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Database.Entities;
+
+    public interface ITripRepository
+    {
+        Task<DbTrip> GetTrip (string tripId);
+
+        Task<IEnumerable<DbEmployeeAmenitiesChecklist>> GetTripChecklistsWithEmployees(string tripId);
+    }
+}
