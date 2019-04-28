a096841d446b1e4baaeb82477384f75d047ea6ec
Martynas <narijauskasmartynas@gmail.com>
2019-04-28 23:57:06 +0300

    DT-39 whoops

--- FrontendApartmentReservation/src/actions/index.jsx
+++ FrontendApartmentReservation/src/actions/index.jsx
@@ -13,7 +13,7 @@ export const GET_TRIP_BASIC = 'GET_TRIP_BASIC';
 
 
 var BACKEND_BASE_URI;
-if (process.env.NODE_ENV !== 'production'){
+if (process.env.NODE_ENV === 'production'){
      BACKEND_BASE_URI = process.env.REACT_APP_PROD_BASE_URI;
 }
 else{
