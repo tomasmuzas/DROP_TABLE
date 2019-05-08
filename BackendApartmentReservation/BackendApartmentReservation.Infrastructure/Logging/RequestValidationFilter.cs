9c373bfbadbe06cdd42821f4ba17effe88e67d98
Tomas Mūžas <m_tomas@inbox.lt>
2019-05-08 23:26:18 +0300

    Minor style changes

--- BackendApartmentReservation/BackendApartmentReservation.Infrastructure/Logging/RequestValidationFilter.cs
+++ BackendApartmentReservation/BackendApartmentReservation.Infrastructure/Logging/RequestValidationFilter.cs
@@ -1,7 +1,5 @@
 ﻿using System.Net;
 using System.Threading.Tasks;
-using System.Web.Http;
-
 using Microsoft.AspNetCore.Mvc.Filters;
 using NLog;
 
@@ -11,12 +9,12 @@ namespace BackendApartmentReservation.Infrastructure.Logging
 
     public class RequestValidationFilter : IAsyncActionFilter
     {
-        private readonly Logger<> _logger = NLog.LogManager.GetCurrentClassLogger();
+        private readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
         public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
         {
             if (!context.ModelState.IsValid)
-            { 
+            {
                 _logger.Error($"Validation failed. Errors: {context.ModelState}");
                 throw new HttpStatusException(HttpStatusCode.BadRequest);
             }
