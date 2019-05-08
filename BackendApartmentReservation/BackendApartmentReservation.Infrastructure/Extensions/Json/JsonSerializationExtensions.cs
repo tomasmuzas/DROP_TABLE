ac11cfa458a30187c38cb240057ff35d981709a4
Tomas Mūžas <m_tomas@inbox.lt>
2019-05-08 23:14:00 +0300

    Move extensions and error response to infrastructure

---
+++ BackendApartmentReservation/BackendApartmentReservation.Infrastructure/Extensions/Json/JsonSerializationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace BackendApartmentReservation.Extensions.Json
+{
+    public static class JsonSerializationExtensions
+    {
+        private static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            MaxDepth = 2
+        };
+
+        public static string ToJson(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj, Settings);
+        }
+    }
+}
