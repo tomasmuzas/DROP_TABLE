21a50e896cbf575eae1de4d1461cfcb3b83be535
Tomas Mūžas <tomu@mobilelife.dk>
2019-05-09 10:24:56 +0300

    Add SecurityManager

---
+++ BackendApartmentReservation/BackendApartmentReservation/Authentication/SecurityManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using BackendApartmentReservation.Infrastructure.Utilities;
+
+namespace BackendApartmentReservation.Authentication
+{
+    public class SecurityManager
+    {
+        public static string GetPasswordHash(Password password)
+        {
+            var hash = SHA256.Create();
+            var passwordBytes = Encoding.UTF8.GetBytes(password);
+            var hashedPasswordBytes = hash.ComputeHash(passwordBytes);
+            return BitConverter.ToString(hashedPasswordBytes);
+        }
+    }
+}
