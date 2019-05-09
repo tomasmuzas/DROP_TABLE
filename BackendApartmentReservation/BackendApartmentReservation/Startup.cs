39cbb80ce272806be6fda8abfe0653fab90e6b0e
Tomas Mūžas <tomu@mobilelife.dk>
2019-05-09 10:40:30 +0300

    Add authentication to the pipeline

--- BackendApartmentReservation/BackendApartmentReservation/Startup.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Startup.cs
@@ -1,4 +1,8 @@
-﻿namespace BackendApartmentReservation
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace BackendApartmentReservation
 {
     using System.IO;
     using Autofac;
@@ -36,6 +40,26 @@
 
         public void ConfigureServices(IServiceCollection services)
         {
+            var key = Encoding.ASCII.GetBytes(Configuration["JwtTokenSecret"]);
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
+
+
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
                 .AddMvcOptions(options => options.Filters.Add(new MethodCallLoggingFilter()))
