e2e828b80e960a4fec763c816b48e356c225c7e1
Tomas Mūžas <m_tomas@inbox.lt>
2019-05-13 08:49:36 +0300

    Make car number nullable

--- BackendApartmentReservation/BackendApartmentReservation/Migrations/20190513053801_MakeCarNumberNullable.Designer.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Migrations/20190513053801_MakeCarNumberNullable.Designer.cs
@@ -4,14 +4,16 @@ using BackendApartmentReservation.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BackendApartmentReservation.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190513053801_MakeCarNumberNullable")]
+    partial class MakeCarNumberNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -341,8 +343,7 @@ namespace BackendApartmentReservation.Migrations
 
                     b.Property<string>("CarAddress");
 
-                    b.Property<string>("CarNumber")
-                        .IsRequired(false);
+                    b.Property<string>("CarNumber").IsRequired(false);
 
                     b.Property<DateTime?>("RentEndTime");
 
