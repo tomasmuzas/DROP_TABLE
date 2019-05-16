9320afd44b07694207f8e4d36a89ec4569e128c8
Ieva <racylaite.ieva@gmail.com>
2019-05-16 23:00:15 +0300

    Create apartment repository

--- BackendApartmentReservation/BackendApartmentReservation/Apartments/ApartmentRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Apartments/ApartmentRepository.cs
@@ -1,7 +1,12 @@
 ﻿namespace BackendApartmentReservation.Apartments
 {
+    using BackendApartmentReservation.Database.Entities;
     using Database;
     using Interfaces;
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
 
     public class ApartmentRepository : IApartmentRepository
     {
@@ -10,6 +15,45 @@
         public ApartmentRepository(DatabaseContext db)
         {
             _db = db;
+        }
+
+        public async Task<DbRoomReservation> CreateRoomReservation(DbEmployee employee, DateTime dateFrom, DateTime dateTo)
+        {
+            var roomReservation = new DbRoomReservation();
+            roomReservation.Employee = employee;
+            roomReservation.DateFrom = dateFrom;
+            roomReservation.DateTo = dateTo;
+
+            var availableRooms = new List<DbApartmentRoom>();
+            foreach (DbApartmentRoom room in _db.ApartmentRooms)
+            {
+                var isAvailable = _db.DbRoomReservations
+                    .Where(e => e.Room.Id == room.Id)
+                    .All(e => e.DateFrom.Date > dateTo.Date || e.DateTo.Date < dateFrom.Date);
+                if (isAvailable)
+                    availableRooms.Add(room);
+            }
+
+            roomReservation.Room = availableRooms.FirstOrDefault();
+            if (roomReservation.Room == null)
+                throw new ArgumentNullException();
+
+            await _db.DbRoomReservations.AddAsync(roomReservation);
+            await _db.SaveChangesAsync();
+
+            return roomReservation;
+        }
+
+        public async Task UpdateRoomReservation(DbRoomReservation roomReservation)
+        {
+            _db.DbRoomReservations.Update(roomReservation);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteRoomReservation(DbRoomReservation roomReservation)
+        {
+            _db.DbRoomReservations.Remove(roomReservation);
+            await _db.SaveChangesAsync();
         }
     }
 }
--- BackendApartmentReservation/BackendApartmentReservation/Apartments/Interfaces/IApartmentRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Apartments/Interfaces/IApartmentRepository.cs
@@ -1,6 +1,15 @@
 ﻿namespace BackendApartmentReservation.Apartments.Interfaces
 {
+    using BackendApartmentReservation.Database.Entities;
+    using System;
+    using System.Threading.Tasks;
+
     public interface IApartmentRepository
     {
+        Task<DbRoomReservation> CreateRoomReservation(DbEmployee employee, DateTime dateFrom, DateTime dateTo);
+
+        Task UpdateRoomReservation(DbRoomReservation roomReservation);
+
+        Task DeleteRoomReservation(DbRoomReservation roomReservation);
     }
 }
