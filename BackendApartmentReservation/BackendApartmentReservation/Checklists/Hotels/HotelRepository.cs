3b193a44e52082b0f77876815352669b753f8f61
Martynas Struckus <martynas.struckus@bentley.com>
2019-05-16 11:55:04 +0300

    Hotel reservation CRUD

--- BackendApartmentReservation/BackendApartmentReservation/Checklists/Hotels/HotelRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Checklists/Hotels/HotelRepository.cs
@@ -1,7 +1,9 @@
 ﻿namespace BackendApartmentReservation.Checklists.Hotels
 {
+    using Database.Entities.Reservations;
     using Database;
     using Interfaces;
+    using System.Threading.Tasks;
 
     public class HotelRepository : IHotelRepository
     {
@@ -10,6 +12,28 @@
         public HotelRepository(DatabaseContext db)
         {
             _db = db;
+        }
+
+        public async Task<DbHotelReservation> CreateEmptyHotelReservation()
+        {
+            var hotelReservation = new DbHotelReservation();
+
+            await _db.HotelReservations.AddAsync(hotelReservation);
+            await _db.SaveChangesAsync();
+
+            return hotelReservation;
+        }
+
+        public async Task UpdateHotelReservation(DbHotelReservation hotelReservation)
+        {
+            _db.Update(hotelReservation);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task DeleteHotelReservation(DbHotelReservation hotelReservation)
+        {
+            _db.HotelReservations.Remove(hotelReservation);
+            await _db.SaveChangesAsync();
         }
     }
 }
