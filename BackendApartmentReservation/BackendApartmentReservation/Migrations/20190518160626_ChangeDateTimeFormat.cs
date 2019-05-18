6cda200233652c680d0eb33333670e6b5ba35e12
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-18 19:07:04 +0300

    Add migration

---
+++ BackendApartmentReservation/BackendApartmentReservation/Migrations/20190518160626_ChangeDateTimeFormat.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendApartmentReservation.Migrations
+{
+    public partial class ChangeDateTimeFormat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ReturnDate",
+                table: "Trips",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DepartureDate",
+                table: "Trips",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTo",
+                table: "HotelReservations",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateFrom",
+                table: "HotelReservations",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "FlightTime",
+                table: "FlightReservations",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTo",
+                table: "DbRoomReservations",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateFrom",
+                table: "DbRoomReservations",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "RentStartTime",
+                table: "CarReservations",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "RentEndTime",
+                table: "CarReservations",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnDate",
+                table: "Trips",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DepartureDate",
+                table: "Trips",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTo",
+                table: "HotelReservations",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateFrom",
+                table: "HotelReservations",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "FlightTime",
+                table: "FlightReservations",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTo",
+                table: "DbRoomReservations",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateFrom",
+                table: "DbRoomReservations",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RentStartTime",
+                table: "CarReservations",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RentEndTime",
+                table: "CarReservations",
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldNullable: true);
+        }
+    }
+}
