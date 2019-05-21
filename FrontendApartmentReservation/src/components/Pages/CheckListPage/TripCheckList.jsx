8e2704a5ebfc3a3608c2b9c8018a585fbd13a0d6
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-22 00:10:15 +0300

    Display more trip information

--- FrontendApartmentReservation/src/components/Pages/CheckListPage/TripCheckList.jsx
+++ FrontendApartmentReservation/src/components/Pages/CheckListPage/TripCheckList.jsx
@@ -24,6 +24,14 @@ class TripCheckList extends React.Component {
         if (this.props.tripbasic && this.props.tripbasic.checklistInfos && this.props.tripbasic.tripId === this.props.match.params.tripId) {
             return (
                 <div>
+                    <h2 className="row mt-5 mx-5">{t("TripDestination")} {this.props.tripbasic.office.address}</h2>
+                    <h5 className="row mt-2 mx-5">{t("TripStart")} {new Date(this.props.tripbasic.startTime).toLocaleDateString("lt-LT")} {t("TripEnd")} {new Date(this.props.tripbasic.endTime).toLocaleDateString("lt-LT")}</h5>
+                    { this.props.tripbasic.availableApartments >= this.props.tripbasic.checklistInfos.length &&
+                        <span className="row mt-5 mx-5" style={{"display": "inline-block"}}> 
+                            {t("TripEnoughApartments")} 
+                            <Link className={`btn btn-primary mx-3`} to={''} type="submit">Reserve for all</Link>
+                        </span> 
+                    }
                     <div>
                         {this.props.tripbasic.checklistInfos.map((checkListInfo, index) =>
                             <CheckListCard checkListInfo={checkListInfo} index={index} tripId={this.props.tripbasic.tripId} key={checkListInfo.employee.id} />)}
