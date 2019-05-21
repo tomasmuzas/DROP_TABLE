17a842a5e698bddd9c3c4fac9d29d7990747524a
Martynas Struckus <martynas.struckus@bentley.com>
2019-05-21 17:39:51 +0300

    Implement merge trips endpoint

---
+++ BackendApartmentReservation/BackendApartmentReservation/DataContracts/DataTransferObjects/Requests/MergeTripsRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BackendApartmentReservation.DataContracts.DataTransferObjects.Requests
+{
+    public class MergeTripsRequest
+    {
+        public string FirstTripId { get; set; }
+        public string SecondTripId { get; set; }
+    }
+}
