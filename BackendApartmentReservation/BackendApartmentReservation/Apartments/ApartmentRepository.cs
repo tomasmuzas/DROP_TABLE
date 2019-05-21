a22928364dfdd6801647accd03282c34b245fe45
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-22 01:17:19 +0300

    Add trip apartment filter to available rooms API

--- BackendApartmentReservation/BackendApartmentReservation/Apartments/ApartmentRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Apartments/ApartmentRepository.cs
@@ -8,6 +8,7 @@
     using System.Threading.Tasks;
     using System.Linq;
     using BackendApartmentReservation.Infrastructure.Exceptions;
+    using Microsoft.EntityFrameworkCore;
 
     public class ApartmentRepository : IApartmentRepository
     {
@@ -18,14 +19,14 @@
             _db = db;
         }
 
-        public async Task<DbRoomReservation> CreateRoomReservation(DbEmployee employee, DateTimeOffset dateFrom, DateTimeOffset dateTo)
+        public async Task<DbRoomReservation> CreateRoomReservation(string tripId, DbEmployee employee, DateTimeOffset dateFrom, DateTimeOffset dateTo)
         {
             var roomReservation = new DbRoomReservation();
             roomReservation.Employee = employee;
             roomReservation.DateFrom = dateFrom;
             roomReservation.DateTo = dateTo;
 
-            var availableRooms = await GetAvailableRooms(dateFrom, dateTo);
+            var availableRooms = await GetAvailableRooms(tripId, dateFrom, dateTo);
 
             roomReservation.Room = availableRooms.FirstOrDefault();
             if (roomReservation.Room == null)
@@ -53,8 +54,19 @@
 
         public async Task<IEnumerable<DbApartmentRoom>> GetAvailableRooms(string tripId, DateTimeOffset dateFrom, DateTimeOffset dateTo)
         {
+            var trip = await _db.Trips
+                .Where(t => t.ExternalTripId == tripId)
+                .Include(t => t.DestinationOffice)
+                    .ThenInclude(o => o.OfficeApartment)
+                        .ThenInclude(a => a.Rooms)
+                .SingleOrDefaultAsync();
+
+            var office = trip.DestinationOffice;
+
+            var rooms = office.OfficeApartment.Rooms;
+
             var availableRooms = new List<DbApartmentRoom>();
-            foreach (DbApartmentRoom room in _db.ApartmentRooms)
+            foreach (var room in rooms)
             {
                 var isAvailable = !_db.DbRoomReservations
                     .Where(r => r.Room.Id == room.Id)
@@ -68,7 +80,5 @@
 
             return availableRooms;
         }
-
-
     }
 }
--- BackendApartmentReservation/BackendApartmentReservation/Apartments/Interfaces/IApartmentRepository.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Apartments/Interfaces/IApartmentRepository.cs
@@ -2,13 +2,13 @@
 
 namespace BackendApartmentReservation.Apartments.Interfaces
 {
-    using BackendApartmentReservation.Database.Entities;
+    using Database.Entities;
     using System;
     using System.Threading.Tasks;
 
     public interface IApartmentRepository
     {
-        Task<DbRoomReservation> CreateRoomReservation(DbEmployee employee, DateTimeOffset dateFrom, DateTimeOffset dateTo);
+        Task<DbRoomReservation> CreateRoomReservation(string tripId, DbEmployee employee, DateTimeOffset dateFrom, DateTimeOffset dateTo);
 
         Task UpdateRoomReservation(DbRoomReservation roomReservation);
 
