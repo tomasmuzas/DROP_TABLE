bd1091570ca899cf537b4f5fd7b5e8a6da844791
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-22 01:29:33 +0300

    Add room count to trip info

--- BackendApartmentReservation/BackendApartmentReservation/Trips/TripInformationManager.cs
+++ BackendApartmentReservation/BackendApartmentReservation/Trips/TripInformationManager.cs
@@ -8,18 +8,22 @@
     using DataContracts.DataTransferObjects.IntermediaryDTOs;
     using DataContracts.DataTransferObjects.Responses;
     using Interfaces;
+    using LivingPlace.Interfaces;
 
     public class TripInformationManager : ITripInformationManager
     {
         private readonly ITripRepository _tripRepository;
         private readonly IConfirmationRepository _confirmationRepository;
+        private readonly ILivingPlaceManager _livingPlaceManager;
 
         public TripInformationManager(
             ITripRepository tripRepository,
-            IConfirmationRepository confirmationRepository)
+            IConfirmationRepository confirmationRepository,
+            ILivingPlaceManager livingPlaceManager)
         {
             _tripRepository = tripRepository;
             _confirmationRepository = confirmationRepository;
+            _livingPlaceManager = livingPlaceManager;
         }
 
         public async Task<BasicTripInformationResponse> GetBasicTripInformation(string tripId)
@@ -45,12 +49,15 @@
                 })
                 .ToList();
 
+            var availableRooms = await
+                _livingPlaceManager.GetNumberOfAvailableApartmentRooms(tripId, trip.DepartureDate, trip.ReturnDate);
+
             return new BasicTripInformationResponse
             {
                 TripId = tripId,
                 StartTime = trip.DepartureDate,
                 EndTime = trip.ReturnDate,
-
+                AvailableApartments = availableRooms,
                 ChecklistInfos = checklistInformations
             };
         }
