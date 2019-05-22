461fe5e16f13473d4858e5ed67907001ec8e4203
Martynas <narijauskasmartynas@gmail.com>
2019-05-23 01:47:33 +0300

    DT-116 ech

--- FrontendApartmentReservation/src/actions/index.jsx
+++ FrontendApartmentReservation/src/actions/index.jsx
@@ -22,7 +22,7 @@ export const CLEAR_MERGEABLE_TRIPS = "CLEAR_MERGEABLE_TRIPS";
 export const GET_MERGEABLE_TRIPS = "GET_MERGEABLE_TRIPS";
 
 var BACKEND_BASE_URI;
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     BACKEND_BASE_URI = process.env.REACT_APP_PROD_BASE_URI;
 }
 else {
