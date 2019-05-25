6a8eff598ff3a6ee0380d218004300e68c7877b7
Tomas Mūžas <muzas.tomas@gmail.com>
2019-05-25 23:50:07 +0300

    Make HotelName nullable

---
+++ BackendApartmentReservation/BackendApartmentReservation/Migrations/20190525204650_MakeHotelNameNullable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackendApartmentReservation.Migrations
+{
+    public partial class MakeHotelNameNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HotelName",
+                table: "HotelReservations",
+                nullable: true,
+                oldNullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HotelName",
+                table: "HotelReservations",
+                nullable: false,
+                oldNullable: true);
+        }
+    }
+}
